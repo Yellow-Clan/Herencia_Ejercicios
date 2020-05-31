6fff62de317863e14cb891caa535a80ac0eb38a9
Frank Wilson <frankwilson.mamani@upeu.edu.pe>
2020-05-31 11:16:24 -0500

    frank branch

--- src/Herencia/Principal.java
+++
@@ -1,8 +0,0 @@
-package Herencia;
-
-public class Principal {
-    public static void main(String[] args) {
-        Estudiante estudiante = new Estudiante("Frank", "MW", 21, 321, 16.6f);
-        estudiante.mostrarDatos();
-    }
-}
